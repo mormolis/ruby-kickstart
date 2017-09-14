a20ac74a68dec11ba363ef8ba2933a2ec33368f4
Georgios <george.lamprakis@gmail.com>
2017-09-14 12:23:58 +0100

    exercise 1 session 3 added

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -3,3 +3,15 @@
 #
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+def reverse_map(*arg)
+    arg = arg.reverse
+    i=0
+    while i<arg.size
+        arg[i] = yield(arg[i])
+        i+=1
+    end
+    return arg
+end
+
+
+# puts reverse_map(1,2,3){|i|  i *2}
