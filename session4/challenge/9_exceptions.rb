3cfe3cff93b8e093d0c365c4ac2f17ca9e687018
Georgios <george.lamprakis@gmail.com>
2017-09-18 16:35:31 +0100

    Exercise 9 Session 4

--- session4/challenge/9_exceptions.rb
+++ session4/challenge/9_exceptions.rb
@@ -13,4 +13,11 @@
 # why that error was raised.
 
 def full_name(person)
+    begin
+        "#{person.first_name} #{person.last_name}"
+    rescue FirstNameError
+        person.last_name
+    rescue LastNameError
+        person.first_name
+    end
 end
