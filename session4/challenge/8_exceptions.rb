e019f14d9a87c635f48c5be74139cff077f4ddd9
Georgios <george.lamprakis@gmail.com>
2017-09-18 16:23:26 +0100

    Exercise 8 Session 4 Added

--- session4/challenge/8_exceptions.rb
+++ session4/challenge/8_exceptions.rb
@@ -12,3 +12,22 @@
 #   exception_raiser 3   # =>   #<Exception: No 3s allowed!>
 #   exception_raiser 4   # =>   #<SyntaxError: No 4s allowed!>
 #   exception_raiser 5   # =>   #<RubyKickstartException: No 5s allowed!>
+
+class RubyKickstartException < StandardError
+end
+def exception_raiser (num)
+    case num
+        when 1
+            raise(RuntimeError, "No 1s allowed!")
+        when 2
+            raise(ArgumentError, "No 2s allowed!")
+        when 3
+            raise(Exception, "No 3s allowed!")
+        when 4
+            raise(SyntaxError, "No 4s allowed!")
+        when 5
+            raise(RubyKickstartException, "No 5s allowed!")
+    end
+    
+end
+
