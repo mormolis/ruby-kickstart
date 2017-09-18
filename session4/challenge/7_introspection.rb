5c3315b9f8ee6c9ee4d49f68acfa1c505b737f8c
Georgios <george.lamprakis@gmail.com>
2017-09-18 16:11:02 +0100

    Exercise 7 Session 4 added

--- session4/challenge/7_introspection.rb
+++ session4/challenge/7_introspection.rb
@@ -4,3 +4,22 @@
 # end
 #
 # longest_method o # => :this_is_a_really_really_really_really_really_long_method_name
+
+
+#arr = [:asda, :sdasd, :werwe]
+def longest_method(obj)
+    arr = obj.methods
+    arr_max=arr[0]
+    arr.each_index do |i|
+        if arr[i].length > arr_max.length
+            arr_max = i
+        end
+    end
+    arr_max
+end
+    
+    
+# o=String.new
+# def o.this_is_a_really_really_really_really_really_long_method_name
+# end
+# p longest_method(o)
